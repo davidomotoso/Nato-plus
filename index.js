0e41c695ac62fcca4d272047fac34d478353f602
davidomotoso <davidomotoso45@gmail.com>
2023-11-28 17:59:15 +0100

    commented out a non commercial api

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const url = "https://marvelstefan-skliarovv1.p.rapidapi.com/getCharacters";
-const options = {
-  headers: {
-    "X-RapidAPI-Key": "60d67beefemsh3a62bd4f2074e44p10bb9bjsn99310101c0d7",
-    "X-RapidAPI-Host": "Marvelstefan-skliarovV1.p.rapidapi.com",
-  },
-};
+// Commented out RapidApi as it was against there policy
 
-fetch(url, options)
-  .then((data) => {
-    return data.json();
-  })
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err.message));
+// const url = "https://marvelstefan-skliarovv1.p.rapidapi.com/getCharacters";
+// const options = {
+//   headers: {
+//     "X-RapidAPI-Key": "60d67beefemsh3a62bd4f2074e44p10bb9bjsn99310101c0d7",
+//     "X-RapidAPI-Host": "Marvelstefan-skliarovV1.p.rapidapi.com",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((data) => {
+//     return data.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.error(err.message));
