bae2fb7fc35b02ddfe8df0da9d2cbf5117af9568
davidomotoso <davidomotoso45@gmail.com>
2023-12-01 18:02:26 +0100

    dummy content

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-// Commented out RapidApi as it was against there policy
+// Making a dummy api for top trending comic
 
-// const url = "https://marvelstefan-skliarovv1.p.rapidapi.com/getCharacters";
-// const options = {
-//   headers: {
-//     "X-RapidAPI-Key": "60d67beefemsh3a62bd4f2074e44p10bb9bjsn99310101c0d7",
-//     "X-RapidAPI-Host": "Marvelstefan-skliarovV1.p.rapidapi.com",
-//   },
-// };
+// getting DOM element
+const animeContainer = document.querySelector(".anime-content>section");
 
-// fetch(url, options)
-//   .then((data) => {
-//     return data.json();
-//   })
-//   .then((data) => console.log(data))
-//   .catch((err) => console.error(err.message));
+// Declaring comic array
+const comics = [
+  {
+    heading: " A comic story of wonder woman. A dummy project tho...",
+    src: "./assets/wonder woman.png",
+    figClass: "wonder-woman",
+    alt: "wonder woman image",
+  },
+  {
+    heading: " A comic story of superman. A dummy project tho...",
+    src: "./assets/superman.png",
+    figClass: "superman",
+    alt: "superman image",
+  },
+  {
+    heading: " A comic story of batman. A dummy project tho...",
+    src: "./assets/batman.png",
+    figClass: "batman",
+    alt: "batman image",
+  },
+  {
+    heading: " A comic story of ironman. A dummy project tho...",
+    src: "./assets/iron-man.png",
+    figClass: "ironman",
+    alt: "ironman image",
+  },
+  {
+    heading: " A comic story of captain america. A dummy project tho...",
+    src: "./assets/captain america.png",
+    figClass: "captain-america",
+    alt: "captain america image",
+  },
+];
+
+const loopComic = (parentComponent) => {
+  let html = "";
+  comics.forEach((comic) => {
+    html += `
+        <section class="anime-intro">
+          <figure class="action-figure ${comic.figClass}">
+            <img src="${comic.src}" alt="${comic.alt}"/>
+          </figure>
+          <section class="comic-story">
+          ${comic.heading}
+          </section>
+          <button>Read more</button>
+        </section>
+        `;
+  });
+  parentComponent.innerHTML = html;
+};
+
+loopComic(animeContainer);
