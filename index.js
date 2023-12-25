8f3f5ccdbcd9b37a4724abcb86f9c7ad34086ff8
davidomotoso <davidomotoso45@gmail.com>
2023-12-25 18:10:58 +0100

    fixed styling and js error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const ts = 1701689634218;
 const publicKey = "02e1f0a2d49b93ccbd50739d6b726c5c";
 const hash = "e0d6a8e188272e010b434feb7a1444cb";
-const URL = `http://gateway.marvel.com/v1/public//series?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+const URL = `https://gateway.marvel.com/v1/public//series?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
 // getting DOM element
 const animeContainer = document.querySelector(".anime-content>section");
