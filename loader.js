3150937a775ec0f3f9c9ce0511cbf9a81e7868d1
davidomotoso <davidomotoso45@gmail.com>
2024-01-02 02:23:40 +0100

    added loader to the anime-intro section

---
+++ loader.js
@@ -0,0 +1,21 @@
+// This JS file is for the loaders
+
+// Declaring DOM elements
+let animeImageLoader = document.querySelector(".anime-intro-loader");
+
+function dummyAnimeElements() {
+  let elements = "";
+  for (let i = 5; i > 0; i--) {
+    elements += `
+    <section>
+        <section>
+          <div></div>
+          <div></div>
+          <div></div>
+        </section>
+        </section>
+    `;
+  }
+  animeImageLoader.innerHTML += elements;
+}
+dummyAnimeElements();
