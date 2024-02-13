3dd76b83be66053dbe137b6dfc7e65a21aca2ac0
davidomotoso <davidomotoso45@gmail.com>
2024-02-13 16:36:17 +0100

    intergrated the loaders

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const headContent = () => {
   <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="loader.css" />
   `;
 };
 
